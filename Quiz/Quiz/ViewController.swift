6697e18806cf8948e110493f4e59db4a2ed76df2
Nicholas Mahlangu <cubemasternm@gmail.com>
2016-01-12 21:43:31 -0800

    Created the interface layer for Quiz app

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Nicholas Mahlangu on 1/12/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // outlets; properties
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    // actions
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
+    }
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+    }
+
+}
+
