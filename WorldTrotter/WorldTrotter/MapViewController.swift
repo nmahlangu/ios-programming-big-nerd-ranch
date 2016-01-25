3c6e26288651318dabeebc33d684866b10a6af09
Nicholas Mahlangu <cubemasternm@gmail.com>
2016-01-25 15:24:09 -0500

    Got 'Locate' button working in WorldTrotter map view

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    var trackingLocation = false
     
     override func loadView() {
         // Create a map view
         mapView = MKMapView()
         
         // Set it as *the* view of this view controller
-        view = mapView
+        self.view = mapView
         
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
@@ -27,16 +30,54 @@ class MapViewController: UIViewController {
         segmentedControl.addTarget(self, action: "mapTypeChanged:", forControlEvents: .ValueChanged)
         
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(segmentedControl)
+        self.view.addSubview(segmentedControl)
         
         let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
-        let margins = view.layoutMarginsGuide
+        let margins = self.view.layoutMarginsGuide
         let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
         let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
         
         topConstraint.active = true
         leadingConstraint.active = true
         trailingConstraint.active = true
+    }
+    
+    override func viewDidLoad() {
+        // always call super
+        super.viewDidLoad()
+        
+        // location auth stuff
+        locationManager.requestAlwaysAuthorization()    // ask for authorization from user
+        locationManager.requestWhenInUseAuthorization() // for use in foreground
+        
+        // add a button
+        let button = UIButton()
+        button.frame = CGRectMake(100, 100, 70, 30)
+        button.setTitle("Locate", forState: UIControlState.Normal)
+        button.backgroundColor = UIColor.blueColor()
+        button.addTarget(self, action: "locationButtonWasPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    func locationButtonWasPressed(sender:UIButton!) {
+        if !self.trackingLocation {
+            if CLLocationManager.locationServicesEnabled() {
+                self.locationManager.delegate = self
+                self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+                self.locationManager.startUpdatingLocation()
+                mapView.showsUserLocation = true
+                self.trackingLocation = true
+            }
+        }
+        else {
+            if CLLocationManager.locationServicesEnabled() {
+                let region: MKCoordinateRegion = MKCoordinateRegionMake(mapView.centerCoordinate, MKCoordinateSpanMake(180, 360))
+                mapView.setRegion(region, animated: true)
+                self.locationManager.stopUpdatingLocation()
+                mapView.showsUserLocation = false
+                self.trackingLocation = false
+            }
+        }
     }
     
     func mapTypeChanged(segControl: UISegmentedControl) {
@@ -50,5 +91,13 @@ class MapViewController: UIViewController {
         default:
             break
         }
+    }
+    
+    // delegate that does all location management
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
     }
 }
