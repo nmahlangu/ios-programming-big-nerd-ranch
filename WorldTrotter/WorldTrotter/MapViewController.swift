d2ef00e80aa3a0b49ae8067b7fadb10ec46d2ba7
Nicholas Mahlangu <cubemasternm@gmail.com>
2016-01-26 13:20:40 -0500

    Completed Gold Challenge - Dropping Pins for GlobeTrotter

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -16,6 +16,12 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     var locationManager = CLLocationManager()
     var trackingLocation = false
     
+    // pin locations
+    let pinCoordinateOne = CLLocationCoordinate2DMake(48.2081743, 16.3738189)
+    let pinCoordinateTwo = CLLocationCoordinate2DMake(42.3736158, -71.1097335)
+    let pinCoordinateThree = CLLocationCoordinate2DMake(48.856614, 2.3522219)
+    var visitingPin: Int = -1
+    
     override func loadView() {
         // Create a map view
         mapView = MKMapView()
@@ -50,13 +56,36 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         locationManager.requestAlwaysAuthorization()    // ask for authorization from user
         locationManager.requestWhenInUseAuthorization() // for use in foreground
         
-        // add a button
+        // add a locate button
         let button = UIButton()
         button.frame = CGRectMake(UIScreen.mainScreen().bounds.width - 90, UIScreen.mainScreen().bounds.height - 100, 70, 30)
         button.setTitle("Locate", forState: UIControlState.Normal)
         button.backgroundColor = UIColor.blueColor()
         button.addTarget(self, action: "locationButtonWasPressed:", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(button)
+        
+        // add a button for visiting pins
+        let pinButton = UIButton()
+        pinButton.frame = CGRectMake(UIScreen.mainScreen().bounds.width - 90, UIScreen.mainScreen().bounds.height - 150, 70, 30)
+        pinButton.setTitle("Pins", forState: UIControlState.Normal)
+        pinButton.backgroundColor = UIColor.redColor()
+        pinButton.addTarget(self, action: "pinButtonWasPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(pinButton)
+        
+        // drop 3 pins
+        var pins = [MKPointAnnotation]()
+        for _ in 0..<3 {
+            pins.append(MKPointAnnotation())
+        }
+        pins[0].coordinate = self.pinCoordinateOne
+        pins[0].title = "Vienna, Austria"
+        pins[1].coordinate = self.pinCoordinateTwo
+        pins[1].title = "Cambridge, MA"
+        pins[2].coordinate = self.pinCoordinateThree
+        pins[2].title = "Paris, France"
+        for i in 0..<3 {
+            mapView.addAnnotation(pins[i])
+        }
     }
     
     func locationButtonWasPressed(sender:UIButton!) {
@@ -77,6 +106,35 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                 mapView.showsUserLocation = false
                 self.trackingLocation = false
             }
+        }
+    }
+    
+    func pinButtonWasPressed(sender:UIButton) {
+        // stop tracking location if user wants to view pins
+        if self.trackingLocation {
+            self.locationManager.stopUpdatingLocation()
+            self.trackingLocation = false
+        }
+        
+        let regionOne = MKCoordinateRegion(center: self.pinCoordinateOne, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        let regionTwo = MKCoordinateRegion(center: self.pinCoordinateTwo, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        let regionThree = MKCoordinateRegion(center: self.pinCoordinateThree, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        switch self.visitingPin {
+        case -1:
+            mapView.setRegion(regionOne, animated: true)
+            self.visitingPin = 0
+        case 0:
+            mapView.setRegion(regionTwo, animated: true)
+            self.visitingPin = 1
+        case 1:
+            mapView.setRegion(regionThree, animated: true)
+            self.visitingPin = 2
+        case 2:
+            mapView.setRegion(regionOne, animated: true)
+            self.visitingPin = 0
+        default:
+            break
         }
     }
     
